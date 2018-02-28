d387510e10c5124ede21a10e6277890ee313ccef
nguyenannie <tramtramrhinomo@gmail.com>
2018-02-28 13:19:42 +0100

    Initial commit

---
+++ src/main/java/com/annie/reddit/RedditApplication.java
@@ -0,0 +1,12 @@
+package com.annie.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditApplication.class, args);
+	}
+}
