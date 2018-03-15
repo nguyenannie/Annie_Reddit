eb973942642bacffdd194cd809dda54a4c7fd8e2
nguyenannie <tramtramrhinomo@gmail.com>
2018-03-15 11:45:13 +0100

    clean up code

--- src/main/java/com/annie/reddit/RedditApplication.java
+++ src/main/java/com/annie/reddit/RedditApplication.java
@@ -9,4 +9,5 @@ public class RedditApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedditApplication.class, args);
 	}
+	
 }
--- src/main/java/com/annie/reddit/services/RedditUserServiceDbImpl.java
+++ src/main/java/com/annie/reddit/services/RedditUserServiceDbImpl.java
@@ -45,4 +45,5 @@ public class RedditUserServiceDbImpl implements RedditUserService {
     }
     return user;
   }
+
 }
